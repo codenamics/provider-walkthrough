259adee198af91c8d1626faeeca97ba19f10fb57
Damian Bauer <damianbauer@MacBook-Pro-Damian.local>
2019-12-23 23:51:11 +0100

    up

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+
 import './cart.dart';
 
 class OrderItem {
@@ -20,53 +22,62 @@ class OrderItem {
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
 
+
+
+
   List<OrderItem> get orders {
     return [..._orders];
   }
 
   Future<void> fetchAndSetOrders() async {
-    const url = 'https://flutter-prov.firebaseio.com/orders.json';
+    final url = 'https://flutter-prov.firebaseio.com/orders.json';
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
     if (extractedData == null) {
       return;
     }
-
     extractedData.forEach((orderId, orderData) {
-      loadedOrders.add(OrderItem(
+      loadedOrders.add(
+        OrderItem(
           id: orderId,
           amount: orderData['amount'],
           dateTime: DateTime.parse(orderData['dateTime']),
-          products: (orderData['products'] as List<dynamic>).map((item) {
-            CartItem(
-                id: item['id'],
-                price: item['price'],
-                quantity: item['quantity'],
-                title: item['title']);
-          }).toList()));
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                      id: item['id'],
+                      price: item['price'],
+                      quantity: item['quantity'],
+                      title: item['title'],
+                    ),
+              )
+              .toList(),
+        ),
+      );
     });
     _orders = loadedOrders.reversed.toList();
-
     notifyListeners();
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    const url = 'https://flutter-prov.firebaseio.com/orders.json';
+       final url = 'https://flutter-prov.firebaseio.com/orders.json';
     final timestamp = DateTime.now();
-    final response = await http.post(url,
-        body: json.encode({
-          'amount': total,
-          'dateTime': timestamp.toIso8601String(),
-          'products': cartProducts
-              .map((cp) => {
-                    'id': cp.id,
-                    'title': cp.title,
-                    'quantity': cp.quantity,
-                    'price': cp.price
-                  })
-              .toList()
-        }));
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
+      }),
+    );
     _orders.insert(
       0,
       OrderItem(
